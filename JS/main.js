799f50dee7a9ac94e480b8e51562caaeab1efa62
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2024-06-26 15:57:28 -0300

    pp

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ const textoResultado = document.querySelector(".texto-resultado")
 
 
 
-const caixaPerguntas = [
+const Perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
