8c60c9dea0e1392ccd10d1e1c90cc0fa5ec0a900
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2024-06-26 16:14:19 -0300

    55

--- JS/main.js
+++ JS/main.js
@@ -9,25 +9,21 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const Perguntas = [
     {
-        enunciado: "Quais são as  soluções que a escola está buscando  para a inclusão social, bem como as questões relacionadas aos direitos dos povos originários e como isso afeta os impactos causados pelo desmatamento ?
-",
+        enunciado: "Quais são as  soluções que a escola está buscando  para a inclusão social, bem como as questões relacionadas aos direitos dos povos originários e como isso afeta os impactos causados pelo desmatamento ?",
         alternativas: [
             "Técnicas alternativas inovadoras",
             "Ensino multidisciplinar"
         ]
     },
     {
-        enunciado: "Qual o papel fundamental dos povos indígenas na luta contra a crise climática e preservação ambiental?
-",
+        enunciado: "Qual o papel fundamental dos povos indígenas na luta contra a crise climática e preservação ambiental?",
         alternativas: [
             "Eles têm mostrado que é possível preservar a biodiversidade, manter os serviços ecossistêmicos e acalmar os efeitos das mudanças climáticas.",
-            "Eles protegem e promovem a biodiversidade através de práticas tradicionais de manejo sustentável dos recursos naturais.
-",
+            "Eles protegem e promovem a biodiversidade através de práticas tradicionais de manejo sustentável dos recursos naturais.",
         ]
     },
     {
-        enunciado: "Você acredita que  o conhecimento da cultura desenvolvida nas escola indígenas pode melhorar o entendimento da comunidade  em relação às mudanças climáticas atuais ?
-",
+        enunciado: "Você acredita que  o conhecimento da cultura desenvolvida nas escola indígenas pode melhorar o entendimento da comunidade  em relação às mudanças climáticas atuais ?",
         alternativas: [
             "Sim, eu acredito que pode ser uma grande contribuição.",
             "Não, pois o método empregado não é benéfico para o meio ambiente."
@@ -46,5 +42,5 @@ const Perguntas = [
             "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
         ]
-    },
+    }
 ];
