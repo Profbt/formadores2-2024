735a7984093addab683b4a295e145cb3b0bada9b
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2024-07-03 15:57:08 -0300

    rr

--- JS/main.js
+++ JS/main.js
@@ -32,7 +32,7 @@ const perguntas = [
    let atual = 0;
    let perguntaAtual;
    function mostraPergunta() {
-    perguntaAtual = caixaPerguntas[atual];
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
    }
 
